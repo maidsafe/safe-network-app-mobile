f0fdc9b6d073277e33d20f30392b7e1d17dc1e01
RavinderJangra <ravinderjangra@live.com>
2019-11-27 11:45:25 +0530

    chore(refactor): remove reperated xaml compilation code and some txt files

--- MobileSnapp/MobileSnapp/Views/Onboarding/LoginPage.xaml.cs
+++ MobileSnapp/MobileSnapp/Views/Onboarding/LoginPage.xaml.cs
@@ -8,11 +8,9 @@
 // Software.
 
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace MobileSnapp.Views.Onboading
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
         public LoginPage()
