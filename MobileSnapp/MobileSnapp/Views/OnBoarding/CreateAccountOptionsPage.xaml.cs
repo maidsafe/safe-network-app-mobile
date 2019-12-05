1b6cb17c6f40c2a9a5d5f8eb980f9483dab89d52
Ravinder Jangra <ravinderjangra@live.com>
2019-12-05 14:20:35 +0530

    chore(create account): add create account onboarding navigation labels

---
+++ MobileSnapp/MobileSnapp/Views/OnBoarding/CreateAccountOptionsPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using MobileSnapp.ViewModels.Onboarding;
+using Xamarin.Forms;
+
+namespace MobileSnapp.Views.OnBoarding
+{
+    public partial class CreateAccountOptionsPage : ContentPage
+    {
+        private CreateAccountOptionsViewModel _viewModel;
+
+        public CreateAccountOptionsPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (_viewModel == null)
+            {
+                _viewModel = new CreateAccountOptionsViewModel(Navigation);
+                BindingContext = _viewModel;
+            }
+        }
+    }
+}
