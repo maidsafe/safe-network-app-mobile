ffe613948b5f124e759539edbf708f77737a456d
Ravinder Jangra <ravinderjangra@live.com>
2019-12-05 14:50:47 +0530

    feat(create account options): add create account options page

--- MobileSnapp/MobileSnapp/ViewModels/Onboarding/CreateAccountOptionsViewModel.cs
+++ MobileSnapp/MobileSnapp/ViewModels/Onboarding/CreateAccountOptionsViewModel.cs
@@ -1,6 +1,17 @@
-﻿using System.Collections.ObjectModel;
+﻿// Copyright 2019 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under the MIT license <LICENSE-MIT
+// https://opensource.org/licenses/MIT> or the Modified BSD license <LICENSE-BSD
+// https://opensource.org/licenses/BSD-3-Clause>, at your option. This file may not be copied,
+// modified, or distributed except according to those terms. Please review the Licences for the
+// specific language governing permissions and limitations relating to use of the SAFE Network
+// Software.
+
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 using MobileSnapp.Helpers;
 using MobileSnapp.Models.Onboarding;
+using MobileSnapp.Views.Onboarding;
 using MvvmHelpers;
 using Xamarin.Forms;
 
@@ -21,6 +32,8 @@ namespace MobileSnapp.ViewModels.Onboarding
                 ContentHelpers.PopulateData<CreateAccountOnboardingStaticContent>(
                     _staticContentFile)
                 .CreateAccountOptionList);
+            RedeemInviteCommand =
+                new Command(() => _navigation.PushModalAsync(new NavigationPage(new CreateAccountPage())));
         }
 
         public ObservableCollection<CreateAccountOnboardingItemWithDesktopOption> CreateAccountOptions
@@ -28,5 +41,7 @@ namespace MobileSnapp.ViewModels.Onboarding
             get => _createAccountOptions;
             set => SetProperty(ref _createAccountOptions, value);
         }
+
+        public ICommand RedeemInviteCommand { get; }
     }
 }
