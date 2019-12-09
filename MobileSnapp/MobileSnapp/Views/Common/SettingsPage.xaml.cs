baa0eb036a343c06afe08ba423debfc781b5472c
Ravinder Jangra <ravinderjangra@live.com>
2019-12-09 18:28:10 +0530

    feat(settings): add settings page + basic view model

---
+++ MobileSnapp/MobileSnapp/Views/Common/SettingsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using MobileSnapp.ViewModels.Common;
+using Xamarin.Forms;
+
+namespace MobileSnapp.Views.Common
+{
+    public partial class SettingsPage : ContentPage
+    {
+        private SettingsViewModel _viewModel;
+
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (_viewModel == null)
+            {
+                _viewModel = new SettingsViewModel(Navigation);
+                BindingContext = _viewModel;
+            }
+        }
+    }
+}
