6a7a8afe4467b8dfde98e53f8316263501e80418
Ravinder Jangra <ravinderjangra@live.com>
2019-12-18 16:07:29 +0530

    chore(indicatorview): iOS exp flag for the indicatorview

--- MobileSnapp/MobileSnapp.iOS/AppDelegate.cs
+++ MobileSnapp/MobileSnapp.iOS/AppDelegate.cs
@@ -26,7 +26,8 @@ namespace MobileSnapp.IOS
         // You have 17 seconds to return from this method, or iOS will terminate your application.
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            Forms.SetFlags("CarouselView_Experimental", "IndicatorView_Experimental");
+            Forms.SetFlags("CarouselView_Experimental");
+            Forms.SetFlags("IndicatorView_Experimental");
             global::Xamarin.Forms.Forms.Init();
             global::Xamarin.Forms.FormsMaterial.Init();
             LoadApplication(new App());
